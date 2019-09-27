653ed104fe06b8dcae795880e111652f417d9148
Jessica Sills <jsills3110@gmail.com>
2019-09-26 20:53:46 -0700

    Added a function for clearing the results when the clear results button is clicked. Added variables for certain html elements.

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var searchTerm = $("#search-term");
+var numberOfRecords = $("#number-of-records");
+var startYear = $("#startYear");
+var endYear = $("endYear");
+var resultsDiv = $("#results");
+
+$("#clear-results").on("click", function() {
+    resultsDiv.empty();
+})
