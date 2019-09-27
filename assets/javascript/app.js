5786639db6dedee100e6506704040830b84d32f1
Jessica Sills <jsills3110@gmail.com>
2019-09-26 21:17:46 -0700

    Test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
-var searchTerm = $("#search-term");
+var searchTerm = $("#search-term").text;
 var numberOfRecords = $("#number-of-records");
 var startYear = $("#startYear");
 var endYear = $("endYear");
 var resultsDiv = $("#results");
+
+var apiKey = "3hN1Jc95ELtBFJb52lnctDanO9AwA1yd";
 
 $("#clear-results").on("click", function() {
     resultsDiv.empty();
